847c6f442d725662325e102e42e04686674909fa
Vinicius Oliveira <vini.asoliveira1@gmail.com>
2022-02-09 18:29:49 -0300

    Delete main.py

--- main.py
+++
@@ -1,3 +0,0 @@
-print("Welcome to the Band Name Generator.")
-print("Your band name could be " + input("What's name of the city you grew up in?\n") + " " +
-      input("What's your pet's name?\n"))
