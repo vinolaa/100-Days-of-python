6fa328dc941d234ef18474b9c1e97f09a9256646
Vinicius Oliveira <vini.asoliveira1@gmail.com>
2022-02-11 19:55:13 -0300

    Own Tresure Map
    Put the X in the map where you want

---
+++ Day4/own_tresure_map.py
@@ -0,0 +1,12 @@
+row1 = ["⬜️", "⬜️", "⬜️"]
+row2 = ["⬜️", "⬜️", "⬜️"]
+row3 = ["⬜️", "⬜️", "⬜️"]
+treasure_map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+
+column = int(position[0]) - 1
+row = int(position[1]) - 1
+treasure_map[row][column] = "X"
+
+print(f"{row1}\n{row2}\n{row3}")
