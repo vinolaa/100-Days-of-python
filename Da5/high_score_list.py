ddc996f8263d2354d65fcaa151287132b6416787
Vinicius Oliveira <vini.asoliveira1@gmail.com>
2022-02-13 19:20:25 -0300

    High Score List
    input: List of integers
    output: The highest number of the list

---
+++ Da5/high_score_list.py
@@ -0,0 +1,9 @@
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+
+high_score = 0
+for high in student_scores:
+    if high > high_score:
+        high_score = high
+print(f"The highest score in the class is: {high_score}")
