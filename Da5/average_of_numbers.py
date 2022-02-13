d70b64e1702bfcd80bf623d17e5237674012b711
Vinicius Oliveira <vini.asoliveira1@gmail.com>
2022-02-13 19:19:15 -0300

    Average of Numbers
    input: List of numbers
    output: Average of the list

---
+++ Da5/average_of_numbers.py
@@ -0,0 +1,9 @@
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+
+heights_sum = 0
+for a in student_heights:
+    heights_sum += a
+media = round(heights_sum / (n + 1))
+print(media)
